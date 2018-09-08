9f4297d00b84ac5cf54fc3680e867c03df889f79
manyu252 <42141004+manyu252@users.noreply.github.com>
2018-09-08 17:27:31 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,32 @@
+(function() {
+	var canvas = document.getElementById('canvas'),
+    	context = canvas.getContext('2d'),
+    	video = document.getElementById('video');
+    	vendorURL = window.URL || window.webkitURL;
+
+	navigator.getMedia = navigator.mediaDevices.getUserMedia || navigator.mozGetUserMedia;
+
+	navigator.getUserMedia({
+		video: true,
+		audio: false
+	}, function(stream) {
+		video.src = vendorURL.createObjectURL(stream);
+	}, function(error) {
+		// An error occured
+		// error.code
+	});    
+
+	//video.src = "Arduino Programming Tutorial- Visual studio and serial com port communctions (1).mp4";
+	//video.type = "video/mp4";
+
+	video.onplay = function()
+	{
+		draw();
+	};
+
+	function draw()
+	{
+		context.drawImage(video, 150, 200, 200, 200, 0, 0, 400, 400);
+		setTimeout(draw,2);
+	}
+})();
